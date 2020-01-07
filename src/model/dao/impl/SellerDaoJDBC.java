ecacc55a89d24b8c9f9f28e5ab716f2256a3d565
Gabriel Araujo <aquacamus@localhost.localdomain>
2020-01-07 13:49:03 -0300

    Atualizando - findByDepartment e findAll

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.mysql.cj.xdevapi.Statement;
 
 import db.DB;
 import db.DbException;
@@ -22,7 +27,32 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		
+		
+		try {
+			st = conn.prepareStatement("INSERT INTO seller\n" + 
+					"		(Name, Email, BirthDate, BaseSalary, DepartmentId)\n" + 
+					"		VALUES\n" + 
+					"		(?, ?, ?, ?, ?)", java.sql.Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5,obj.getDepartment().getId());
+			
+			int linhasAfetadas = st.executeUpdate();
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			
+		}
+		
 		
 	}
 
@@ -87,8 +117,115 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n" + 
+					"FROM seller INNER JOIN department\n" + 
+					"ON seller.DepartmentId = department.Id\n" + 
+					"ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<Seller>();
+			
+			//Conjunto está vazio
+			//Vai guardar a ID e um Departamento
+			Map<Integer, Department> map = new HashMap<>();
+			
+			
+			//Enquanto o ResultSet retornar valores
+			while (rs.next()) {
+				
+				//Na primeira iteração, não haverá departamento algum no map
+				//O método get() retornará null
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				//Se dep for nulo
+				if (dep == null) {
+					
+					//Instancia com base nos valores do ResultSet
+					dep = instantiateDepartment(rs);
+					
+					//Adiciona o dep instanciado ao map, sendo o Id desse dep o mesmo que estiver no ResultSet
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				//Passado o teste, instanciar o vendedor passando o ResultSet e o departamento
+				Seller obj = instantiateSeller(rs, dep);
+				
+				//Adicionar vendedor à lista
+				list.add(obj);
+			}
+		//Retornar a lista preenchida
+		return list;			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n" + 
+					"FROM seller INNER JOIN department\n" + 
+					"ON seller.DepartmentId = department.Id\n" + 
+					"WHERE DepartmentId = ?\n" + 
+					"ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<Seller>();
+			
+			//Conjunto está vazio
+			//Vai guardar a ID e um Departamento
+			Map<Integer, Department> map = new HashMap<>();
+			
+			
+			//Enquanto o ResultSet retornar valores
+			while (rs.next()) {
+				
+				//Na primeira iteração, não haverá departamento algum no map
+				//O método get() retornará null
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				//Se dep for nulo
+				if (dep == null) {
+					
+					//Instancia com base nos valores do ResultSet
+					dep = instantiateDepartment(rs);
+					
+					//Adiciona o dep instanciado ao map, sendo o Id desse dep o mesmo que estiver no ResultSet
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				//Passado o teste, instanciar o vendedor passando o ResultSet e o departamento
+				Seller obj = instantiateSeller(rs, dep);
+				
+				//Adicionar vendedor à lista
+				list.add(obj);
+			}
+		//Retornar a lista preenchida
+		return list;			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
